beb5fe261709d869053cdf2066582e3dc01ec10f
xWesleyMartins <wesley_869@hotmail.com.br>
2022-04-11 15:50:42 -0300

    exercicio 5

---
+++ Modulos/Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicio5.js
@@ -0,0 +1,15 @@
+// Faça um programa que defina três constantes com os valores dos três ângulos internos de um triângulo. Retorne true se os ângulos representarem os ângulos de um triângulo e false , caso contrário. Se algum ângulo for inválido o programa deve retornar uma mensagem de erro
+
+const a = 60
+const b = 50
+const c = 80
+if (a+b+c > 180){
+    console.log("Erro! 'Angulo informado invalido'")
+}
+if (a+b+c === 180){
+    console.log("True")
+}else{
+    console.log("False")
+}
+
+console.log("O angulo atual somado informado é "+ (a+b+c));
