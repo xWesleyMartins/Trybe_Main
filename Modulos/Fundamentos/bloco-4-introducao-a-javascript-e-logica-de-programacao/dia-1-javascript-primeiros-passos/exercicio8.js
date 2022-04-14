163c901f4e1b3ce04b6af0a6b94d771b2ce84340
xWesleyMartins <wesley_869@hotmail.com.br>
2022-04-14 15:40:30 -0300

    realizando testes exercicios

---
+++ Modulos/Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicio8.js
@@ -0,0 +1,11 @@
+const num1=4;
+const num2=2;
+const num3=4;
+const valorNumero = [num1,num2, num3];
+
+if (valorNumero.length % 2 === 0){
+    console.log("true");
+}else{
+    console.log("false")
+}
+console.log(valorNumero);
---
+++ Modulos/desafio.js
@@ -0,0 +1,16 @@
+/* Crie uma função que, dado um array de números inteiros, retorne a quantidade de números pares e ímpares no formato: 
+{
+ pares: 0, 
+ ímpares: 0 
+} 
+*/
+
+//let numeros = [1,2,3,4,5,6,7,8,9,10];
+    function paresImpares (numeros){
+    for (let index=0;index < numeros.length; index +=1) {
+
+    }
+
+
+
+}
